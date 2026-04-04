1250ddee15e16ccbc080dd4da5821c52ba211512
jaine-rodrigues <jainerodrigues1706@gmail.com>
2026-04-04 02:06:21 -0300

    feature/style

---
+++ script.js
@@ -0,0 +1,15 @@
+
+const botao = document.querySelector('.btn')
+const menulateral = document.querySelector('.menu-lateral')
+const conteudo = document.querySelector('.conteudo')
+const backgroud = document.querySelector('.backgroud')
+
+botao.addEventListener('click', () => {
+    menulateral.classList.add('ativo')
+    botao.classList.add('ativo')
+    conteudo.classList.add('ativo')
+    backgroud.classList.add('ativo')
+    document.body.style.backgroundColor = menulateral.classList.contains['ativo'] ?' #34495e' : '#ecf0f1 '
+
+    
+})
